64a960cb966802a5998736c87262c438caacd449
Tanner Davison <tanner.davison95@gmail.com>
2025-12-01 16:32:34 -0700

    saving multithreading dynamics

--- source/main.cpp
+++ source/main.cpp
@@ -16,6 +16,11 @@ public:
   };
 };
 
+struct Greeter {
+public:
+  void hello() { std::cout << "saying hello" << std::endl; };
+};
+
 void thread_one(std::vector<Person> &&persons) {
   int count{1};
 
@@ -41,5 +46,22 @@ int main() {
   std::vector<Person> persons{abdul, bart, claudia, divya};
   std::thread thr1(thread_one, std::move(persons));
   thr1.join();
+
+  Greeter greet;
+  std::thread thr(&Greeter::hello, &greet);
+  thr.join();
+
+  int i = 1;
+  std::thread thr2([&i] { i *= 2; });
+  thr2.join();
+  std::cout << "\nint i was 1 and is now = " << i << "\n";
+
+  std::thread thr3(
+      [](int a, int b) {
+        std::cout << "the sum of " << a << " + " << b << " is " << a + b
+                  << "\n";
+      },
+      4, 19);
+  thr3.join();
   return 0;
 }
