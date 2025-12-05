eb31a26db5170fc751a00ae510405e4bdf3157fc
Tanner Davison <tanner.davison95@gmail.com>
2025-12-05 15:37:12 -0700

    multhithreaded functions

--- source/main.cpp
+++ source/main.cpp
@@ -1,68 +1,28 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
-#include <vector>
+using namespace std::literals;
 
-class Person {
-public:
-  std::string name;
-  Person(std::string _name) : name(_name) {}
+void hello() { std::cout << "Started hello Thread!()\n"; }
 
-  void operator()(int arg) const {
-    std::cout << this->name << " says " << arg << "\n";
-  };
+void wait_sec(int seconds) {
 
-  void operator()(std::string arg) const {
-    std::cout << this->name << " says " << arg << "\n";
-  };
+  std::cout << "\n...waiting..\n" << std::flush;
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
+  hello();
 };
+void func(std::thread obj) {
+  std::cout << "\nRecieved Thread Object..\n";
 
-struct Greeter {
-public:
-  void hello() { std::cout << "saying hello" << std::endl; };
-};
-
-void thread_one(std::vector<Person> &&persons) {
-  int count{1};
-
-  while (count <= 20) {
-    for (auto &person : persons) {
-      if (count % 3 == 0 && count % 5 == 0) {
-        person("FizzBuzz!");
-      } else if (count % 3 == 0) {
-        person("Fizz!");
-
-      } else if (count % 5 == 0) {
-        person("Buzz!");
-      } else {
-        person(count);
-      }
-      ++count;
-    }
-  }
+  obj.join();
 }
 
 int main() {
-  Person abdul{"Abdul"}, bart{"Bart"}, claudia{"Claudia"}, divya{"Divya"};
-  std::vector<Person> persons{abdul, bart, claudia, divya};
-  std::thread thr1(thread_one, std::move(persons));
-  thr1.join();
 
-  Greeter greet;
-  std::thread thr(&Greeter::hello, &greet);
-  thr.join();
-
-  int i = 1;
-  std::thread thr2([&i] { i *= 2; });
-  thr2.join();
-  std::cout << "\nint i was 1 and is now = " << i << "\n";
-
-  std::thread thr3(
-      [](int a, int b) {
-        std::cout << "the sum of " << a << " + " << b << " is " << a + b
-                  << "\n";
-      },
-      4, 19);
-  std::cout << "Thread 3's Native handle : " << thr3.get_id() << std::endl;
-  thr3.join();
+  std::cout << "Starting thread ...\n";
+  std::thread thr(wait_sec, 2);
+  std::thread thr4(hello);
+  func(std::move(thr));
+  func(std::move(thr4));
   return 0;
 }
