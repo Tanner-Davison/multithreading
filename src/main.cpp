12ebfeabd7dffefb0d48907b3b58c62f791927ef
Tanner Davison <tanner.davison95@gmail.com>
2025-12-09 16:53:21 -0700

    added mutex locking class example

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,51 @@
 using namespace std::literals;
 
 class Counter {
-private:
-  std::atomic<int> count{0};
+  private:
+    std::atomic<int> count{0};
 
-public:
-  void increment() { ++count; }
-  int get() const { return count.load(); }
+  public:
+    void increment() {
+        ++count;
+    }
+    int get() const {
+        return count.load();
+    }
+};
+
+class MutexCounter {
+  private:
+    int        count = 0;
+    std::mutex task_mtx;
+
+  public:
+    void increment() {
+        task_mtx.lock();
+        ++count;
+        task_mtx.unlock();
+    }
+    int getcount() {
+        return count;
+    }
 };
 
 int main() {
-  Counter counter;
+    MutexCounter counter;
 
-  auto incrementLots = [&counter]() {
-    for (int i = 0; i < 100000; ++i) {
-      counter.increment();
+    auto incrementLots = [&counter]() {
+        for (int i = 0; i < 100000; ++i) {
+            counter.increment();
+        };
     };
-  };
 
-  std::thread t1(incrementLots);
-  std::thread t2(incrementLots);
-  std::thread t3(incrementLots);
+    std::thread t1(incrementLots);
+    std::thread t2(incrementLots);
+    std::thread t3(incrementLots);
 
-  t1.join();
-  t2.join();
-  t3.join();
+    t1.join();
+    t2.join();
+    t3.join();
 
-  std::cout << "\n" << counter.get() << std::endl;
-  return 0;
+    std::cout << "\n" << counter.getcount() << std::endl;
+    return 0;
 }
