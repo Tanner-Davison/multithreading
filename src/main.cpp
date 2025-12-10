05545da557bd7cb3772a463fe402a02f7c4e5f94
Tanner Davison <tanner.davison95@gmail.com>
2025-12-09 18:05:01 -0700

    shows three versions of mutex locking strategies

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 using namespace std::literals;
 
-class Counter {
+class AtomicCounter {
   private:
     std::atomic<int> count{0};
 
@@ -21,7 +21,7 @@ class Counter {
     }
 };
 
-class MutexCounter {
+class MutexLocker {
   private:
     int        count = 0;
     std::mutex task_mtx;
@@ -31,41 +31,72 @@ class MutexCounter {
         std::lock_guard<std::mutex> lock(task_mtx);
         ++count;
     }
-    int getcount() {
+    int get() {
         return count;
+    }
+};
+class MutexTryLock {
+  private:
+    int              count = 0;
+    std::mutex       mtx_task;
+    std::atomic<int> attempt_unlock_count{0};
+
+  public:
+    void increment() {
+        while (!mtx_task.try_lock()) {
+            attempt_unlock_count++;
+        }
+        count++;
+        mtx_task.unlock();
+    }
+    int get() {
+        std::lock_guard<std::mutex> lock(mtx_task);
+        return count;
+    }
+    int get_attempts() {
+        std::lock_guard<std::mutex> lock(mtx_task);
+        return attempt_unlock_count;
     }
 };
 
 int main() {
-    Counter      counter;
-    MutexCounter counter_mtx;
+    AtomicCounter counter;
+    MutexLocker   counter_mtx;
+    MutexTryLock  counter_try;
 
-    auto incrementLots = [&counter]() {
+    auto AtomicIncrement = [&counter]() {
         for (int i = 0; i < 100000; ++i) {
             counter.increment();
         };
     };
 
-    auto incrementLots_mtx = [&counter_mtx]() {
+    auto MutextLockerIncrement = [&counter_mtx]() {
         for (int i = 0; i < 100000; ++i) {
             counter_mtx.increment();
         };
     };
-    std::thread t1(incrementLots);
-    std::thread t2(incrementLots);
-    std::thread t3(incrementLots);
-    std::thread t4(incrementLots_mtx);
-    std::thread t5(incrementLots_mtx);
-    std::thread t6(incrementLots_mtx);
+
+    auto MutexTryLockIncrement = [&counter_try]() {
+        for (int i = 0; i < 100000; ++i) {
+            counter_try.increment();
+        };
+    };
+    std::thread t1(AtomicIncrement);
+    std::thread t2(AtomicIncrement);
+    std::thread t3(MutextLockerIncrement);
+    std::thread t4(MutextLockerIncrement);
+    std::thread t5(MutexTryLockIncrement);
+    std::thread t6(MutexTryLockIncrement);
 
     t1.join();
     t2.join();
-    t3.join();
     std::cout << "\n" << counter.get() << std::endl;
+    t3.join();
     t4.join();
+    std::cout << "\n" << counter_mtx.get() << std::endl;
     t5.join();
     t6.join();
-
-    std::cout << "\n" << counter_mtx.getcount() << std::endl;
+    std::cout << "\n" << counter_try.get() << std::endl;
+    std::cout << "\n" << counter_try.get_attempts() << std::endl;
     return 0;
 }
