4c3fd5fb4d64df49253d9778d340ba18ac16e478
Tanner Davison <tanner.davison95@gmail.com>
2026-01-28 17:02:11 -0700

    better follow through in ordinal rotations

--- src/deadlock_example.cpp
+++ src/deadlock_example.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <map>
 #include <mutex>
 #include <print>
 #include <thread>
@@ -21,14 +23,18 @@ namespace {
  * Prevention: std::scoped_lock acquires BOTH mutexes atomically,
  * preventing the circular wait condition that causes deadlock.
  */
-constexpr int         n_forks                   = 5;
-constexpr int         n_philosophers            = n_forks;
-constexpr std::string names[n_philosophers]     = {"A", "B", "C", "D", "E"};
-int                   mouthfuls[n_philosophers] = {0};
-constexpr auto        think_time                = 100ms;
-constexpr auto        eat_time                  = 50ms;
 
-std::mutex fork_mutex[n_forks];
+bool                             selfish_starter{false};
+constexpr int                    n_forks                   = 5;
+constexpr int                    n_philosophers            = n_forks;
+constexpr std::string            names[n_philosophers]     = {"A", "B", "C", "D", "E"};
+constexpr auto                   think_time                = 100ms;
+constexpr auto                   eat_time                  = 50ms;
+int                              mouthfuls[n_philosophers] = {0};
+std::vector<std::pair<int, int>> finished_positions;
+constexpr std::string_view       ordinals[] = {"", "1st", "2nd", "3rd", "4th", "5th"};
+int                              position{0};
+std::mutex                       fork_mutex[n_forks];
 
 void print(int n_id, const std::string& str, int forkno, int forknotwo) {
     std::println(
@@ -37,22 +43,31 @@ void print(int n_id, const std::string& str, int forkno, int forknotwo) {
 void print(int n_id, const std::string& str) {
     std::println("Philosopher {} {}", names[n_id], str);
 }
+void see_results(int n_id, const std::string& str, int mouthfuls, int position) {
+    std::println("{} {} {} time{} finished in {} place.",
+                 names[n_id],
+                 str,
+                 mouthfuls,
+                 mouthfuls == 1 ? "" : "s",
+                 ordinals[position]);
+}
+
 void dine(int n_philo) {
-    bool selfish_starter = false;
-    int  l_fork{n_philo};
-    int  r_fork{(n_philo + 1) % n_forks};
+    int left_fork{n_philo};
+    int right_fork{(n_philo + 1) % n_forks};
 
     print(n_philo, "is thinking..");
     std::this_thread::sleep_for(think_time);
-    std::scoped_lock lock_fork(fork_mutex[l_fork], fork_mutex[r_fork]);
+    std::scoped_lock lock_fork(fork_mutex[left_fork], fork_mutex[right_fork]);
     if (!selfish_starter) {
         selfish_starter = true;
-        print(n_philo, "Selfishly starts first & Picks up forks ", l_fork, r_fork);
+        print(n_philo, "Selfishly starts first & Picks up forks ", left_fork, right_fork);
     } else {
-        print(n_philo, "Picks up both forks numbered ", l_fork, r_fork);
+        print(n_philo, "Picks up both forks numbered ", left_fork, right_fork);
     }
     std::this_thread::sleep_for(eat_time);
     print(n_philo, "Finished eating");
+    finished_positions.emplace_back(n_philo, ++position);
     std::this_thread::sleep_for(think_time);
 
     mouthfuls[n_philo] += 1;
@@ -69,8 +84,22 @@ void run_philosophers() {
     for (auto& philosophers : table) {
         philosophers.join();
     }
+    // sort before printing to see from A-E
+    std::sort(finished_positions.begin(), finished_positions.end());
+    for (const auto& [philo, position] : finished_positions) {
+        see_results(philo, "ate", mouthfuls[philo], position);
+    }
+}
+void run_with_deadlock() {
+    std::vector<std::thread> table;
+    table.reserve(n_philosophers);
     for (int i = 0; i < n_philosophers; ++i) {
-        std::println("{} was able to eat {} times.", names[i], mouthfuls[i]);
+        table.emplace_back(dine, i);
+    }
+    for (auto& philosophers : table) {
+        philosophers.join();
+    }
+    for (int i = 0; i < n_philosophers; ++i) {
     }
 }
 
