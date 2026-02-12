22f030472ed4a3dcae315101e63d41e02f36957f
Tanner Davison <tanner.davison95@gmail.com>
2026-02-11 20:42:48 -0700

    ex complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,44 +6,35 @@ using namespace std::literals;
 namespace shared_mutex_example {
 void run_demo();
 }
-
 namespace singleton {
 void run_demo();
 }
-
 namespace thread_local_example {
 void run_demo();
 }
-
 namespace recursive_mutex_example {
 int run_bad_factorial();
 }
-
 namespace double_checked_locking {
 void run_safer_call_once();
 void run_double_check_demo();
 } // namespace double_checked_locking
-
 namespace deadlock_example {
 void run_philosophers();
 void run_with_deadlock(); // WARN: runs forever
 } // namespace deadlock_example
-
 namespace pre_seventeen_deadlock_example {
 void run_demo();
 void run_demo_using_defer();
 void run_try_lock_demo();
 } // namespace pre_seventeen_deadlock_example
-
 namespace bank_example {
 void deposit_and_check_balance();
 } // namespace bank_example
-
 namespace livelock_example {
 void run_livelock_demo();
 void run_livelock_eternal_conversation(); // WARN: runs forever
 } // namespace livelock_example
-
 namespace thread_coordination {
 void run_demo();
 } // namespace thread_coordination
