58ec618f690a780053f2478e26028838f2cad501
Tanner Davison <tanner.davison95@gmail.com>
2026-02-15 17:02:43 -0700

    saving changes

--- include/ThreadGuard.hpp
+++ include/ThreadGuard.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include <iostream>
 #include <thread>
-
+/*
+ * Greate project to Write once but essentially just use jthread if you are able to write in c++20
+ * or later
+ * */
 class ThreadGuard {
     std::thread thr;
 
@@ -9,8 +12,8 @@ class ThreadGuard {
     explicit ThreadGuard(std::thread&& p_thr);
 
     ~ThreadGuard(); // joins thread if joinable()
-                    // deleted copy operators to prevent copying
 
+    // deleted copy operators to prevent copying
     ThreadGuard(const ThreadGuard&)            = delete;
     ThreadGuard& operator=(const ThreadGuard&) = delete;
 };
